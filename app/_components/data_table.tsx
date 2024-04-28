aaf1045a458ffdb7f0dd5bca20d9f10c8c9cd1a9
Poonam Minde <poonamminde789@gmail.com>
2024-04-28 23:10:14 +0530

    Resizable Columns

--- app/_components/data_table.tsx
+++ app/_components/data_table.tsx
@@ -16,6 +16,7 @@ import {
     useReactTable,
 } from "@tanstack/react-table";
 import { DataTablePagination } from "./data-table-pagination";
+import { useState, useMemo } from "react";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -23,32 +24,84 @@ interface DataTableProps<TData, TValue> {
 }
 
 export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+    const [isSelected, setIsSelected] = useState({});
+    const [rowSelection, setRowSelection] = useState({});
     const table = useReactTable({
         data,
         columns,
+        defaultColumn: {
+            minSize: 60,
+            maxSize: 800,
+        },
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
+        state: {
+            rowSelection,
+        },
+        enableRowSelection: true, //enable row selection for all rows
+        // enableRowSelection: row => row.original.age > 18, // or enable row selection conditionally per row
+        onRowSelectionChange: setRowSelection,
+        enableMultiRowSelection: false,
+        columnResizeMode: "onChange",
+        debugTable: true,
+        debugHeaders: true,
+        debugColumns: true,
     });
-
+    //state: { rowSelection: rowSelection },
+    //onRowSelectionChange: setRowSelection,
     // TASK : Make first 2 columns (i.e. checkbox and task id) sticky
     // TASK : Make header columns resizable
+    console.log(rowSelection);
+    const columnSizeVars = useMemo(() => {
+        const headers = table.getFlatHeaders();
+        const colSizes: { [key: string]: number } = {};
+        for (let i = 0; i < headers.length; i++) {
+            const header = headers[i]!;
+            colSizes[`--header-${header.id}-size`] = header.getSize();
+            colSizes[`--col-${header.column.id}-size`] = header.column.getSize();
+        }
+        return colSizes;
+    }, [table.getState().columnSizingInfo]);
 
     return (
         <div className="space-y-4">
             <div className="rounded-md border">
-                <Table>
+                <Table
+                    {...{
+                        className: "divTable",
+                        style: {
+                            ...columnSizeVars, //Define column sizes on the <table> element
+                            width: table.getTotalSize(),
+                        },
+                    }}
+                >
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
                             <TableRow key={headerGroup.id}>
                                 {headerGroup.headers.map((header) => {
                                     return (
                                         <TableHead key={header.id} colSpan={header.colSpan}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                      header.column.columnDef.header,
-                                                      header.getContext(),
-                                                  )}
+                                            <div className="flex justify-between">
+                                                {header.isPlaceholder
+                                                    ? null
+                                                    : flexRender(
+                                                          header.column.columnDef.header,
+                                                          header.getContext(),
+                                                      )}
+                                                <div className="flex">
+                                                    {"->"}
+                                                    <div
+                                                        onDoubleClick={() =>
+                                                            header.column.resetSize()
+                                                        }
+                                                        onMouseDown={header.getResizeHandler()}
+                                                        onMouseUp={header.getResizeHandler()}
+                                                        className="hover:bg-violet-600 w-1"
+                                                    >
+                                                        |
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </TableHead>
                                     );
                                 })}
@@ -62,14 +115,20 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
                                     key={row.id}
                                     data-state={row.getIsSelected() && "selected"}
                                 >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext(),
-                                            )}
-                                        </TableCell>
-                                    ))}
+                                    {row.getVisibleCells().map((cell) => {
+                                        //console.log(cell);
+                                        return (
+                                            <TableCell
+                                                key={cell.id}
+                                                onClick={() => setIsSelected(cell)}
+                                            >
+                                                {flexRender(
+                                                    cell.column.columnDef.cell,
+                                                    cell.getContext(),
+                                                )}
+                                            </TableCell>
+                                        );
+                                    })}
                                 </TableRow>
                             ))
                         ) : (
--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,34 @@
 
 import { DataTable } from "./_components/data_table";
 import { useDataFetching } from "./_hooks/useDataFetching";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
 
 export default function HomePage() {
     const { data, columns } = useDataFetching();
-
-    // TASK : Create the feature for selecting rows in DataTable
+    const [isSelected, setIsSelected] = useState({ id: "" });
+    //  : Create the feature for selecting rows in DataTable
     // const { rowSelection, setRowSelection } = useRowSelection();
 
     return (
         <div className="flex flex-col gap-10 items-center p-10">
             <span className="text-3xl text-gray-700 font-bold">Task Details</span>
             <div className="max-w-screen-lg flex size-full flex-col">
-                <DataTable columns={columns} data={data} />
+                <DataTable
+                    columns={[
+                        {
+                            header: () => <Checkbox />,
+                            accessorKey: "checkbox",
+                            cell: ({ row }) => (
+                                <div className="w-200">
+                                    <Checkbox />
+                                </div>
+                            ),
+                        },
+                        ...columns,
+                    ]}
+                    data={data}
+                />
             </div>
         </div>
     );
